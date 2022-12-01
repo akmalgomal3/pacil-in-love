a06c3dc82d5df473908b2a490ccb9ab240696c92
Pramudiptha - <pramudiptha.l@gmail.com>
2022-12-02 00:26:27 +0700

    Try Commit

--- React/views.py
+++ React/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.db import connection
+from django.shortcuts import redirect, render
 
 # Create your views here.
+def index(request):
+    return redirect("home:index")
+    
+def like(request):
+    data = request.POST
+    cursor = connection.cursor()
+    cursor.execute("set search_path to public")
+    
+    username = request.session['username']
+    role = request.session['role']
+    
+    cursor.execute("set search_path to tkyry")
+    
+    return render(request)
+
+def dislike(request):
+    data = request.POST
+    cursor = connection.cursor()
+    cursor.execute("set search_path to public")
+    
+    username = request.session['username']
+    role = request.session['role']
+    
+    cursor.execute("set search_path to tkyry")
+    
+    return render(request)

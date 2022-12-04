019cbda1c2cd81aa013dc5014d5e8e34911f50ce
akmalgomal3 <muhamad.akmal01@ui.ac.id>
2022-12-04 20:55:56 +0700

    Finishing timeout feature

--- ban/views.py
+++ ban/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from django.db import connection
 from Auth.views import *
 from Auth.urls import *
+import datetime
 
 def banPage(request):
     userUsername = checkLoggedIn(request)
@@ -28,6 +29,14 @@ def req_timeout(request, username):
     if request.method == "GET":
         print('Tombol timeout dipencet')
         print(username)
+
+        current_datetime = datetime.datetime.now()
+        print(current_datetime)
+
+        cursor = connection.cursor()
+
+        cursor.execute("SET SEARCH_PATH TO public")
+        cursor.execute("UPDATE pengguna SET timeout = '"+str(current_datetime)+"' WHERE username='"+username+"'")
 
     return HttpResponseRedirect('/ban')
 

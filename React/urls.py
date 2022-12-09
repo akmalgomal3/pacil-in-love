abef42718e8c70bdde55709fd6568b2e525b17dd
BintangNS <bintang.nursyawalli@ui.ac.id>
2022-12-09 20:15:46 +0700

    Refactor URL

--- React/urls.py
+++ React/urls.py
@@ -5,7 +5,6 @@ app_name = 'react'
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('filter/', views.filter, name = 'filter'),
     path('like/<str:user>/<str:id_hobi>', views.like, name='like'),
     path('dislike/<str:id_hobi>', views.dislike, name='dislike'),
     path('<str:hobbyID>', views.filterHobby, name='filterHobby')
